4b2f6313588881ced701b1cb81316c591b19c7c1
alexandre.mendonca <alexandre.mendonca@squadra.com.br>
2024-09-22 02:23:46 -0300

    [UNITTEST] - Unit Tests Use Case Create Sales

---
+++ test/Ambev.Sales.UnitTests/Builders/SalesResponseBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.Sales.Domain.Entities;
+using Ambev.Sales.UnitTests.Utils;
+using Bogus;
+
+namespace Ambev.Sales.UnitTests.Builders
+{
+    public class SalesResponseBuilder
+    {
+        private readonly SalesResponse _instance;
+        private static readonly Faker _faker = FakerPtBr.CreateFaker();
+
+        public SalesResponseBuilder()
+        {
+            _instance = new SalesResponse
+            {
+                SalesId = _faker.Random.String(20)
+            };
+        }
+
+        public SalesResponse Build()
+        {
+            return _instance;
+        }
+    }
+}
