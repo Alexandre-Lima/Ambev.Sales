337b5e8c7078040d837a62c4531a76ee75c9dcc0
alexandre.mendonca <alexandre.mendonca@squadra.com.br>
2024-09-22 02:25:16 -0300

    [INTEGRATIONTEST] - Integration Tests Use Case Create Sales

---
+++ test/Ambev.Sales.IntegrationTest/Builders/SalesResponseBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.Sales.Domain.Entities;
+using Ambev.Sales.IntegrationTest.Utils;
+using Bogus;
+
+namespace Ambev.Sales.IntegrationTest.Builders
+{
+    public class SalesResponseBuilder
+    {
+        private readonly SalesResponse _instance;
+        private static readonly Faker _faker = FakerPtBr.CreateFaker();
+
+        public SalesResponseBuilder()
+        {
+            _instance = new SalesResponse
+            {
+                SalesId = _faker.Random.String(20)
+            };
+        }
+
+        public SalesResponse Build()
+        {
+            return _instance;
+        }
+    }
+}
---
+++ test/Ambev.Sales.IntegrationTest/Controllers/SalesControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Afya.Wedoo.IntegrationTest.Configurations;
+using Ambev.Sales.Domain.Entities;
+using Ambev.Sales.Domain.Repositories;
+using Ambev.Sales.IntegrationTest.Builders;
+using Ambev.Sales.IntegrationTest.Configurations;
+using FluentAssertions;
+using FluentAssertions.Common;
+using Moq;
+using System.Text;
+using System.Text.Json;
+
+namespace Ambev.Sales.IntegrationTest.Controllers
+{
+    public class SalesControllerTest : IntegrationTestBase, IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        public SalesControllerTest(CustomWebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task CreateSales_ShouldReturnSuccess()
+        {
+            // Arrange
+            var request = new CreateSalesRequestBuilder().Build();
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(request), 
+                Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.PostAsync($"/api/sales", content);
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        }
+
+        [Fact]
+        public async Task CreateSales_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var request = new CreateSalesRequestBuilder()
+                .WithSaleNumberIsNull()
+                .Build();
+
+            var content = new StringContent(
+               JsonSerializer.Serialize(request),
+               Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.PostAsync($"/api/sales", content);
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeFalse();
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
