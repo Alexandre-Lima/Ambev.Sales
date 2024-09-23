b97e8c1287a047cf63a89e8c642772a908d4805a
alexandre.mendonca <alexandre.mendonca@squadra.com.br>
2024-09-23 19:24:07 -0300

    [CLEANCODE] - Apply Dry to repository for clean code

--- src/Ambev.Sales.Repositories/SaleRepository.cs
+++ src/Ambev.Sales.Repositories/SaleRepository.cs
@@ -1,89 +1,35 @@
 ﻿using Ambev.Sales.Domain.Entities;
-using Ambev.Sales.Domain.HttpResponse.Models;
 using Ambev.Sales.Domain.Repositories;
-using Newtonsoft.Json;
-using System.Net.Mime;
-using System.Text;
 
 namespace Ambev.Sales.Repositories
 {
-    public class SaleRepository : ISaleRepository
+    public class SaleRepository : ApiRepository, ISaleRepository
     {
-        private readonly HttpClient _httpClient;
-
         public SaleRepository(HttpClient httpClient)
+        : base(httpClient)
         {
-            _httpClient = httpClient;
+        }
+
+        public async Task<SalesRequest?> GetSaleAsync(string saleId)
+        {
+            var url = $"{saleId}?meta=false";
+            return await GetAsync<SalesRequest>(url);
         }
 
         public async Task<SalesResponse> CreateSaleAsync(SalesRequest request)
         {
-            using var content = new StringContent(
-                JsonConvert.SerializeObject(request),
-                Encoding.UTF8,
-                MediaTypeNames.Application.Json);
-
-            using var response = await _httpClient.PostAsync(new Uri("", UriKind.Relative), content);
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var erro = JsonConvert.DeserializeObject<ErrorMessage>(responseContent)!;
-                throw new Exception(erro.Message);
-            }
-
-            var storageResponse = JsonConvert.DeserializeObject<SalesStorageResponse>(responseContent)!;
-
+            var storageResponse = await PostAsync<SalesStorageResponse>($"", request);
             return new SalesResponse { SalesId = storageResponse.Metadata.Id };
         }
 
         public async Task UpdateSaleAsync(SalesRequest request, string saleId)
         {
-            using var content = new StringContent(
-                   JsonConvert.SerializeObject(request),
-                   Encoding.UTF8,
-                   MediaTypeNames.Application.Json);
-
-            using var response = await _httpClient.PutAsync(new Uri(saleId, UriKind.Relative), content);
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var erro = JsonConvert.DeserializeObject<ErrorMessage>(responseContent)!;
-                throw new Exception(erro.Message);
-            }
-        }
-
-        public async Task<SalesRequest?> GetSaleAsync(string saleId)
-        {
-            using var response = await _httpClient.GetAsync(new Uri($"{saleId}?meta=false", UriKind.Relative));
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return null;
-            }
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var erro = JsonConvert.DeserializeObject<ErrorMessage>(responseContent)!;
-                throw new Exception(erro.Message);
-            }
-
-            var responseSale = JsonConvert.DeserializeObject<SalesRequest>(responseContent)!;
-            return responseSale;
+            await PutAsync($"{saleId}", request);
         }
 
         public async Task DeleteSaleAsync(string saleId)
         {
-            using var response = await _httpClient.DeleteAsync(new Uri(saleId, UriKind.Relative));
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var erro = JsonConvert.DeserializeObject<ErrorMessage>(responseContent)!;
-                throw new Exception(erro.Message);
-            }
+            await DeleteAsync($"{saleId}");
         }
     }
 }
